0438b2236c5e452af6ec8a9ab95c3efab69bff95
Sanket <sanket.parmar11@gmail.com>
2015-07-31 01:03:35 +0530

    Added Gruntfile.js & package.json, need to install grunt

--- index.php
+++ index.php
@@ -10,3 +10,38 @@
 	License		: GPLv2 or Later
 	License URI	: https://www.gnu.org/licenses/gpl-2.0.html
  */
+
+/*
+ * The WordPress Location Share Plugin
+ *
+ * @package    WPLocationShare
+ * @subpackage Main
+ */
+
+/*
+ * The server file system path to the plugin directory
+ */
+if( !defined( 'WP_LOCATION_SHARE_PATH' ) ) {
+	define( 'WP_LOCATION_SHARE_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+/*
+ * URL to plugin directory
+ */
+if( !defined( 'WP_LOCATION_SHARE_URL' ) ) {
+	define( 'WP_LOCATION_SHARE_URL', plugin_dir_url( __FILE__ ) );
+}
+
+/*
+ * Plugin basename
+ */
+if( !defined( 'WP_LOCATION_SHARE_BASE_NAME' ) ) {
+	define( 'WP_LOCATION_SHARE_BASE_NAME', plugin_basename( __FILE__ ) );
+}
+
+/*
+ * Plugin Version
+ */
+if( !defined( 'WP_LOCATION_SHARE_VERSION' ) ) {
+	define( 'WP_LOCATION_SHARE_VERSION', '1.0' );
+}
