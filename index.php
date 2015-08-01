f4b369ab53c06f7097d6f5b04c721f3bcc51245b
Sanket <sanket.parmar11@gmail.com>
2015-08-02 02:46:17 +0530

    Fixed plugin header

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /*
-	Plugin Name : WordPress Location Share
-	Plugin URI	: http://ps.com/wp-location-share/?utm_source=dashboard&utm_medium=plugin&utm_campaign=wp-location-share
-	Description	: This plugin shares location.
-	Version		: 1.0
-	Author		: Pranali, Sanket
-	Text Domain	: wplocation
-	Author URI	: http://ps.com/?utm_source=dashboard&utm_medium=plugin&utm_campaign=wp-location-share
-	License		: GPLv2 or Later
-	License URI	: https://www.gnu.org/licenses/gpl-2.0.html
+	Plugin Name: WordPress Location Share
+	Plugin URI:  http://ps.com/wp-location-share/?utm_source=dashboard&utm_medium=plugin&utm_campaign=wp-location-share
+	Description: This plugin shares location.
+	Version: 	 1.0
+	Author: 	 Pranali, Sanket
+	Text Domain: wplocation
+	Author URI:  http://ps.com/?utm_source=dashboard&utm_medium=plugin&utm_campaign=wp-location-share
+	License: 	 GPLv2 or Later
+	License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
