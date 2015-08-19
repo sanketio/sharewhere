106320a161b6ece631730f3a8c741585a5edb67d
Sanket <sanket.parmar11@gmail.com>
2015-08-20 01:00:28 +0530

    Updated plugin details

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 /*
 * Plugin Name: ShareWhere
-* Plugin URI:  https://github.com/sanketio/sharewhere?utm_source=dashboard&utm_medium=plugin&utm_campaign=wp-location-share
+* Plugin URI: https://github.com/sanketio/sharewhere?utm_source=dashboard&utm_medium=plugin&utm_campaign=wp-location-share
 * Description: This plugin shares location.
-* Version: 	   1.0
-* Author: 	   Pranali, Sanket
+* Version: 1.0
+* Author: Pranali, Sanket
 * Text Domain: wpls
-* Author URI:  https://github.com/sanketio/sharewhere?utm_source=dashboard&utm_medium=plugin&utm_campaign=wp-location-share
-* License: 	   GPLv2 or Later
+* Author URI: https://github.com/sanketio/sharewhere?utm_source=dashboard&utm_medium=plugin&utm_campaign=wp-location-share
+* License: GPLv2 or Later
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
